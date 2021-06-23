bd2686a3ef02271805fea6344598c64b00828cfb
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-23 19:02:26 -0300

    resolution of part 4 of the exercise

---
+++ fundamentals/block-04/part-4/scripts.js
@@ -0,0 +1,8 @@
+let nota = 91
+if (nota >= 80) {
+    console.log("Você foi aprovado")
+} else if (nota >= 60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovado")
+}
