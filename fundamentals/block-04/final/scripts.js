9cba333c361516726c731dfbf795163ddce82b15
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-24 09:02:22 -0300

    Requirement 3

--- fundamentals/block-04/final/scripts.js
+++ fundamentals/block-04/final/scripts.js
@@ -19,3 +19,17 @@ if (c > d) {
 } else {
     console.log("'d' é maior do que 'c'");
 }
+
+// Requisito 3
+
+let e = 5;
+let f = 6;
+let g = 7;
+
+if (e > f && e > g) {
+  console.log('O maior número é: ' + e + ' (e)');
+} else if (f > e && f > g) {
+  console.log('O maior número é: ' + f + ' (f)');
+} else {
+  console.log('O maior número é: ' + g + ' (g)');
+}
