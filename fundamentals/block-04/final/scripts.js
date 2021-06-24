d4567febd415370e66fbea11b295d5eadc3c3e2e
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-24 10:10:23 -0300

    Requirement 6

--- fundamentals/block-04/final/scripts.js
+++ fundamentals/block-04/final/scripts.js
@@ -65,3 +65,31 @@ if (h > 0) {
   } else {
     console.log('Erro: ângulo inválido');
   };
+
+  // Requisito 6
+
+  let peca = 'rainha';
+
+  switch (peca.toLowerCase()) {
+    case 'rei': 
+    console.log('Rei -> uma casa apenas para qualquer direção.');
+    break;
+    case 'bispo':
+      console.log('Bispo -> Diagonal');
+    break;
+    case 'rainha':
+      console.log('Rainha-> Diagonal, horizontal e vertical.');
+      break;
+    case 'cavalo':
+      console.log('Cavalo -> "L" pode pular sobre as peças.');
+      break;
+    case 'torre':
+      console.log('Torre -> Horizontal e vertical.');
+      break;
+    case 'peão':
+      console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+      break;
+    default:
+      console.log('Erro: Peça inválida');
+      break;
+  }
