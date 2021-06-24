089a8b40c60724994ac44513224e07dbc7267a54
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-24 08:50:17 -0300

    Requirement 1

---
+++ fundamentals/block-04/final/scripts.js
@@ -0,0 +1,12 @@
+// Requisito 1
+
+let a = 1;
+let b =2;
+
+console.log("Soma: " + (a+b));
+console.log("Subtração: " + (a-b));
+console.log("Multiplicação: " + (a*b));
+console.log("Divisão: " + (a/b));
+console.log("Módulo: " + (a%b));
+
+// Requisito 2
