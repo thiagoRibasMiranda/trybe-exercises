8dc77d6fb35cb3d52762247490b2fac71511d2f3
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-25 15:44:49 -0300

    Requirement 4

--- fundamentals/block-04/day-2/scripts.js
+++ fundamentals/block-04/day-2/scripts.js
@@ -4,4 +4,11 @@ let soma = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index];
 }
-console.log(soma);
+
+let media = soma / numbers.length;
+
+if (media > 20) {
+    console.log ("valor maior que 20");
+}   else {
+    console.log("valor menor ou igual a 20");
+}
