891c4de2c719ed96a546520a9ff8e9a56b4a89fb
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-25 08:53:42 -0300

    Requirement 11

--- fundamentals/block-04/final/scripts.js
+++ fundamentals/block-04/final/scripts.js
@@ -149,3 +149,38 @@ if (custo <= 0 || precoVenda <= 0) {
   let lucro = (precoVenda - valorCustoTotal) * 1000;
   console.log(lucro);
 }
+
+// Requisito 11
+
+let iNSS;
+let iR;
+
+let salarioBruto = 3000;
+
+if (salarioBruto <= 1556.94) {
+  iNSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  iNSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  iNSS = salarioBruto *0.11;
+} else {
+  iNSS = 570.88;
+}
+
+let salarioBase = salarioBruto - iNSS;
+
+if (salarioBase <= 1903.98) {
+  iR = 0
+} else if (salarioBase <= 2826.65) {
+  iR = (salarioBase * 0.075) - 142.80
+}  else if (salarioBase <= 3751.05) {
+  iR = (salarioBase * 0.15) - 354.80
+} else if (salarioBase <= 4664.68) {
+  iR = (salarioBase * 0.225) - 636.13
+} else {
+  iR = (salarioBase * 0.275) - 869.36
+}
+
+let salarioLiquido = salarioBase - iR
+
+console.log(salarioLiquido)
