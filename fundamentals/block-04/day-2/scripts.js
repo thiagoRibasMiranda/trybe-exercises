315696373e740dcaa2496c8cc956e69299e466a0
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-25 15:59:00 -0300

    Requirement 5

--- fundamentals/block-04/day-2/scripts.js
+++ fundamentals/block-04/day-2/scripts.js
@@ -1,14 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0; 
+let maiorNumero = 0 
 
 for (let index = 0; index < numbers.length; index += 1) {
-    soma += numbers[index];
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    } 
 }
 
-let media = soma / numbers.length;
-
-if (media > 20) {
-    console.log ("valor maior que 20");
-}   else {
-    console.log("valor menor ou igual a 20");
-}
+console.log(maiorNumero);
