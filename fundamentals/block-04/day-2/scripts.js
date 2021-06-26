80f80058a7d28b0fcafff267cfbe63e5457a46b3
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-26 08:18:11 -0300

    Requirement 8

--- fundamentals/block-04/day-2/scripts.js
+++ fundamentals/block-04/day-2/scripts.js
@@ -1,15 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let vetor = []
 
-let numerosImpar = 0;
-
-for(let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 != 0) {
-        numerosImpar += 1;
-    }
+for (let index = 0; index < 25; index += 1) {
+    vetor.push(index + 1);
 }
 
-if (numerosImpar === 0) {
-    console.log('nenhum valor ímpar encontrado');
-} else {
-    console.log('A quantidade de números ímpares é igual a: ', numerosImpar);
-};
+console.log(vetor);
