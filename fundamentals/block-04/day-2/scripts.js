9650211aa4f7bd1c451f2be39f0c2a72f3056ddb
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-26 07:59:15 -0300

    Requirement 7

--- fundamentals/block-04/day-2/scripts.js
+++ fundamentals/block-04/day-2/scripts.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = 0 
+let menorNumero = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] > maiorNumero) {
-        maiorNumero = numbers[index];
+    if (numbers[index] < menorNumero) {
+        menorNumero = numbers[index];
     } 
 }
 
-console.log(maiorNumero);
+console.log(menorNumero);
