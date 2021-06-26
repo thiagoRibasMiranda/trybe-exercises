077708fc62b5dcf4968136d787557c7c405cf4ad
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-26 08:37:48 -0300

    Requirement Bonus 1

--- fundamentals/block-04/day-2/scripts.js
+++ fundamentals/block-04/day-2/scripts.js
@@ -1,11 +1,12 @@
-let vetor = []
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < 25; index += 1) {
-    vetor.push(index + 1);
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
 }
-
-console.log(vetor);
-
-for (let index =0; index < vetor.length; index += 1) {
-    console.log (vetor[index] / 2);
-}
+console.log (numbers);
