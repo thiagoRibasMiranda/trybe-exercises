0722e716ace984b44a18208df10534294ee78d80
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-26 08:49:12 -0300

    Requirement Bonus 3

--- fundamentals/block-04/day-2/scripts.js
+++ fundamentals/block-04/day-2/scripts.js
@@ -1,12 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let doubleNumbers = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (numbers[index] > numbers[secondIndex]) {
-      let position = numbers[index];
-      numbers[index] = numbers[secondIndex];
-      numbers[secondIndex] = position;
-    }
+for (let index =0; index < numbers.length; index += 1) {
+  if (index +1 >= numbers.length){
+    doubleNumbers.push(numbers[index] * 2);
+  } else{
+    doubleNumbers.push(numbers[index] * numbers[index + 1]);
   }
-}
-console.log (numbers);
+} 
+console.log(doubleNumbers)
