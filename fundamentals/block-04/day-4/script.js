4e1e4203f509d851915d7fdad0582b95b52b33e0
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-29 10:53:29 -0300

    Requirement 2

--- fundamentals/block-04/day-4/script.js
+++ fundamentals/block-04/day-4/script.js
@@ -4,4 +4,4 @@ let player = {
     age: 34,
     medals: { golden: 2, silver: 3 },
 };
-console.log(player)
+console.log('A jogadora',player.name, player.lastName, 'tem', player.age, 'anos de idade')
