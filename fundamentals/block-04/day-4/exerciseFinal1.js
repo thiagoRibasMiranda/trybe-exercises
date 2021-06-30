7aad92d988e700c4e6fc983c31418bfc40dd446a
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-06-30 10:03:24 -0300

    Requirement 2

--- fundamentals/block-04/day-4/exerciseFinal1.js
+++ fundamentals/block-04/day-4/exerciseFinal1.js
@@ -4,4 +4,7 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log('Bem-vinda, ' + info.personagem)
+  //console.log('Bem-vinda, ' + info.personagem)
+
+info.recorrente = 'Sim';
+console.log(info);
