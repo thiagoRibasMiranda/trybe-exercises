662a49632f84903f3ec5a744dbb4940c2f43dcfe
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-01 08:18:00 -0300

    Requirement 1 of exercise final part 2

---
+++ fundamentals/block-04/day-4/exerciseFinal2.js
@@ -0,0 +1,11 @@
+function verificaPalindrome(palavra) {
+  let palavraInvertida = '';
+  for(let index = palavra.length -1; index >= 0; index -= 1) {
+    palavraInvertida += palavra[index];
+  }
+  if(palavraInvertida === palavra) {
+    return true;
+  } else {
+    return false;
+  }
+} console.log(verificaPalindrome('cachorro'));
