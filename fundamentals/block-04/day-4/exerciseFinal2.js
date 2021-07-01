ea7583353003b352484d701c306a205e39c71141
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-01 08:45:59 -0300

    Requirement 3 of exercise final part 2

--- fundamentals/block-04/day-4/exerciseFinal2.js
+++ fundamentals/block-04/day-4/exerciseFinal2.js
@@ -10,12 +10,22 @@
 //   }
 // } console.log(verificaPalindrome('cachorro'));
 
-function indiceMaiorValor(vetor) {
-  let maior = vetor[0];
-  let posicao
+// function indiceMaiorValor(vetor) {
+//   let maior = vetor[0];
+//   let posicao;
+//   for(i = 1; i < vetor.length; i += 1) {
+//     if(vetor[i] > maior) { 
+//       posicao = i;
+//     }
+//   }  return posicao;
+// } console.log(indiceMaiorValor([2, 3, 6, 7, 10, 1]));
+
+function indiceMenorValor(vetor) {
+  let menor = vetor[0];
+  let posicao;
   for(i = 1; i < vetor.length; i += 1) {
-    if(vetor[i] > maior) { 
-      posicao = i
+    if(vetor[i] < menor) { 
+      posicao = i;
     }
   }  return posicao;
-} console.log(indiceMaiorValor([2, 3, 6, 7, 10, 1]));
+} console.log(indiceMenorValor([2, 4, 6, 7, 10, 0, -3]));
