86afbbc1a2ed707087c0dcd2979e828779ede24f
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-01 08:43:40 -0300

    Requirement 2 of exercise final part 2

--- fundamentals/block-04/day-4/exerciseFinal2.js
+++ fundamentals/block-04/day-4/exerciseFinal2.js
@@ -1,11 +1,21 @@
-function verificaPalindrome(palavra) {
-  let palavraInvertida = '';
-  for(let index = palavra.length -1; index >= 0; index -= 1) {
-    palavraInvertida += palavra[index];
-  }
-  if(palavraInvertida === palavra) {
-    return true;
-  } else {
-    return false;
-  }
-} console.log(verificaPalindrome('cachorro'));
+// function verificaPalindrome(palavra) {
+//   let palavraInvertida = '';
+//   for(let index = palavra.length -1; index >= 0; index -= 1) {
+//     palavraInvertida += palavra[index];
+//   }
+//   if(palavraInvertida === palavra) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// } console.log(verificaPalindrome('cachorro'));
+
+function indiceMaiorValor(vetor) {
+  let maior = vetor[0];
+  let posicao
+  for(i = 1; i < vetor.length; i += 1) {
+    if(vetor[i] > maior) { 
+      posicao = i
+    }
+  }  return posicao;
+} console.log(indiceMaiorValor([2, 3, 6, 7, 10, 1]));
