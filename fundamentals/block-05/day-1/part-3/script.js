9ad95529b6bdde933b784510f6989d78171671bf
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-04 10:00:24 -0300

    Requirement 1 of part 3

---
+++ fundamentals/block-05/day-1/part-3/script.js
@@ -0,0 +1,4 @@
+const paragrafo = document.querySelector('.paragrafo');
+paragrafo.style.color = 'red';
+const paragrafo2 = document.querySelector('#second-paragraph');
+paragrafo2.style.color = 'purple';
