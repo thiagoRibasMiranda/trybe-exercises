0bcee127251bd24bd64e84340a41a4df87697b7e
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-06 08:02:22 -0300

    Requeriment 3

--- fundamentals/block-05/day-2/part-1/script.js
+++ fundamentals/block-05/day-2/part-1/script.js
@@ -3,3 +3,6 @@ const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 // 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 const pai = elementoOndeVoceEsta.parentNode
 pai.style.color = 'red';
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+primeiroFilhoDoFilho.innerText = 'Texto';
