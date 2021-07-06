4fec9cc6ecac42a921a517f795362d1fe84f170b
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-06 07:56:08 -0300

    Requeriment 2

--- fundamentals/block-05/day-2/part-1/script.js
+++ fundamentals/block-05/day-2/part-1/script.js
@@ -1,2 +1,5 @@
 // 1. Acesse o elemento elementoOndeVoceEsta.
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+const pai = elementoOndeVoceEsta.parentNode
+pai.style.color = 'red';
