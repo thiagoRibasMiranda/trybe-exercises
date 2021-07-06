8c9783b9799dd35201c71b2bdd785c595e2f502d
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-06 10:37:14 -0300

    Requeriment 2

--- fundamentals/block-05/day-2/part-4/script.js
+++ fundamentals/block-05/day-2/part-4/script.js
@@ -1,4 +1,8 @@
-//Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
 let h1 = document.createElement('h1');
 h1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(h1);
+// Adicione a tag div com a classe main-content como filho da tag body;
+let mainContent = document.createElement('div');
+mainContent.classList.add('main-content');
+document.body.appendChild(mainContent);
