1c5bf057540d35250288e46d2a0d6b7c051129d0
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-06 09:21:22 -0300

    Requeriment 3 part-2

--- fundamentals/block-05/day-2/part-2/script.js
+++ fundamentals/block-05/day-2/part-2/script.js
@@ -8,3 +8,8 @@ const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 const filhoDoElementoOndeVoceEsta = document.createElement('div');
 filhoDoElementoOndeVoceEsta.id = 'filhoDoElementoOndeVoceEsta';
 elementoOndeVoceEsta.appendChild(filhoDoElementoOndeVoceEsta);
+// 3.Crie um filho para primeiroFilhoDoFilho.
+const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const filhoDoPrimeiroFilhoDoFilho = document.createElement('div');
+filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
