df9836f56fda806e0b8d9c4e9c9700a32965c445
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-06 09:11:22 -0300

    Requeriment 1 part-2

---
+++ fundamentals/block-05/day-2/part-2/script.js
@@ -0,0 +1,5 @@
+// 1. Crie um irmão para `elementoOndeVoceEsta`.
+const pai = document.getElementById('pai');
+const irmaoDoElementoOndeVoceEsta = document.createElement('div');
+irmaoDoElementoOndeVoceEsta.id = 'irmaoDoElementoOndeVoceEsta';
+pai.appendChild(irmaoDoElementoOndeVoceEsta);
