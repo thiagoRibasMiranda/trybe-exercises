0e5bc3968fe88c5ddb9bf9eb19fb89773f54a516
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-14 09:40:25 -0300

    Requirement 2

--- fundamentals/block-05/day-4/part-1/script.js
+++ fundamentals/block-05/day-4/part-1/script.js
@@ -1,5 +1,9 @@
 let divContainerMain = document.getElementById('container-main');
 document.getElementById('button1').addEventListener('click', changeBackgroundColor);
 function changeBackgroundColor(event) {
-	divContainerMain.classList.toggle('pink')
+	divContainerMain.classList.toggle('pink');
+}
+document.getElementById('button2').addEventListener('click', changeColorText);
+function changeColorText(event) {
+	divContainerMain.classList.toggle('white');
 }
