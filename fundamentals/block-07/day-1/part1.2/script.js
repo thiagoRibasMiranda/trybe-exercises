ca796a1f8a0dc4c4c1ff37ba2b14a14865bc914a
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-26 09:42:09 -0300

    Requirement 1 part-1.2

---
+++ fundamentals/block-07/day-1/part1.2/script.js
@@ -0,0 +1,13 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const orderArray = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  // return oddsAndEvens;
+}
+orderArray();
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
