6aadb61a761d951a48998d811d36d98deb33126e
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-27 18:17:24 -0300

    Requirement 8 part-2

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -55,3 +55,13 @@ const valueOfKey = (obj, pos) => {
   return Object.values(obj)[pos];
 }
 console.log(valueOfKey(lesson3, 1));
+
+const verifyPair = (obj, key, value) => {
+  const arr = Object.entries(obj);
+  let isEqual = false;
+  for (let index in arr) {
+    if (arr[index][0] === key && arr[index][1] === value) isEqual = true;
+  }
+  return isEqual;
+};
+console.log(verifyPair(lesson2,'professor','Carlos'));
