8b835143a944f377faf0e30a6b7c7236f5edd7de
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-28 21:15:50 -0300

    Correction Initial commit requirement 4 part-2

--- fundamentals/block-07/day-3/part-2/find-needle.js
+++ fundamentals/block-07/day-3/part-2/find-needle.js
@@ -1,9 +1,17 @@
-const findTheNeedle = (array, word) => {
-  let output = -1;
-  for (const index in array) {
-    if (word === array[index]) {
-      output = Number(index);
-    }
-  }
-  return output;
-};
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
