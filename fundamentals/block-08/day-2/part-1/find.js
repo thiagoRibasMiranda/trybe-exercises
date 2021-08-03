cc2ed5be01509f7d4eee1266e2b7c1f1060109ab
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-03 09:49:31 -0300

    Requirement 2.1 part-1

--- fundamentals/block-08/day-2/part-1/find.js
+++ fundamentals/block-08/day-2/part-1/find.js
@@ -1,7 +1,9 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
+const findDivisibleBy3And5 = (number) => {
   // Adiciona seu código aqui
+  if (number % 3 === 0 && number % 5 === 0) {
+    return number;
+  }
 }
-
-console.log(findDivisibleBy3And5())
+console.log(numbers.find(findDivisibleBy3And5));
