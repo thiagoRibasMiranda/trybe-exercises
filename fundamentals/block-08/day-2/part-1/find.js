e8037588d2fe3da92a9206ba95e4e946f274d3cc
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-03 10:00:59 -0300

    Requirement 2.2 part-1

--- fundamentals/block-08/day-2/part-1/find.js
+++ fundamentals/block-08/day-2/part-1/find.js
@@ -1,3 +1,4 @@
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = (number) => {
@@ -7,3 +8,14 @@ const findDivisibleBy3And5 = (number) => {
   }
 }
 console.log(numbers.find(findDivisibleBy3And5));
+
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+  return names.find((item) => item.length === 5); 
+}
+
+console.log(findNameWithFiveLetters());
