c03f9e132b1eff06082b22b1aaf3a5f1ae2f0b8d
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-03 10:04:38 -0300

    Requirement 2.3 part-1

--- fundamentals/block-08/day-2/part-1/find.js
+++ fundamentals/block-08/day-2/part-1/find.js
@@ -15,7 +15,29 @@ const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
 const findNameWithFiveLetters = () => {
   // Adicione seu código aqui:
-  return names.find((item) => item.length === 5); 
+  return names.find((item) => item.length === 5);
 }
 
 console.log(findNameWithFiveLetters());
+
+// 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+const musicas = [{
+    id: '31031685',
+    title: 'Partita in C moll BWV 997'
+  },
+  {
+    id: '31031686',
+    title: 'Toccata and Fugue, BWV 565'
+  },
+  {
+    id: '31031687',
+    title: 'Chaconne, Partita No. 2 BWV 1004'
+  },
+]
+
+function findMusic(id) {
+  // Adicione seu código aqui
+  return musicas.find((item) => item.id === id);
+}
+
+console.log(findMusic('31031685'))
