25efeff3bf10ad9e39f8053786bd2f1bd29b0eed
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-04 09:17:39 -0300

    Requirement 4.1 part-1.

---
+++ fundamentals/block-08/day-2/part-1/sort.js
@@ -0,0 +1,12 @@
+// 1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente .
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+
+console.log(people);
