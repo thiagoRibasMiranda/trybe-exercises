a314a995e580a13bf2f81a800655a92591c179d5
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-04 10:32:44 -0300

    Exercise3

--- fundamentals/block-08/day-2/part-2/exercise3.js
+++ fundamentals/block-08/day-2/part-2/exercise3.js
@@ -78,6 +78,7 @@ const expectedResult = {
 
 function getNamedBook() {
   // escreva seu código aqui
+  books.find((book) => book.name.length === 26)
 }
 
 assert.deepStrictEqual(getNamedBook(), expectedResult);
