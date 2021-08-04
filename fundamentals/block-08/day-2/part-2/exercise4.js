d39471ceb70409971a67029d8f255a6e1065e13f
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-04 10:44:13 -0300

    Exercise4

--- fundamentals/block-08/day-2/part-2/exercise4.js
+++ fundamentals/block-08/day-2/part-2/exercise4.js
@@ -112,6 +112,7 @@ const expectedResult = [
 
 function booksOrderedByReleaseYearDesc() {
   // escreva aqui seu código
+  return books.sort((book1, book2) => book2.releaseYear - book1.releaseYear);
 }
 
 assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
