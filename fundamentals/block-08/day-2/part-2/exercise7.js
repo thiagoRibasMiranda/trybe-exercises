9bcaea5e18473746fe7283a39ade4aa5c17249ed
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-04 11:37:27 -0300

    Exercise7

--- fundamentals/block-08/day-2/part-2/exercise7.js
+++ fundamentals/block-08/day-2/part-2/exercise7.js
@@ -69,6 +69,7 @@ const expectedResult = false;
 
 function authorUnique() {
   // escreva seu código aqui
+  return books.every((book) => !books.some((bookSome) => (bookSome.author.birthYear === book.author.birthYear) && (bookSome.author.name !== book.author.name)));
 }
 
 assert.strictEqual(authorUnique(), expectedResult);
