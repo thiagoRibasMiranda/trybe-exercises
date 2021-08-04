513ad6078836514c2d992ecb42a09971d2caf524
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-04 10:21:15 -0300

    Exercise2

--- fundamentals/block-08/day-2/part-2/exercise2.js
+++ fundamentals/block-08/day-2/part-2/exercise2.js
@@ -68,7 +68,11 @@ const books = [
 function smallerName() {
   let nameBook;
   // escreva aqui o seu código
-
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
