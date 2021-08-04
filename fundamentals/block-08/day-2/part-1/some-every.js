68297b0a65757074758ac420b23a893ab6511e78
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-04 09:07:44 -0300

    Requirement 3.1 part-1.

--- fundamentals/block-08/day-2/part-1/some-every.js
+++ fundamentals/block-08/day-2/part-1/some-every.js
@@ -4,6 +4,7 @@ const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (arr, name) => {
   //Adicione seu código aqui
+  return arr.some((item) => item === name);
 }
 
-console.log(hasName(names, 'Ana'))
+console.log(hasName(names, 'Ana'));
