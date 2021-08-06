4f144139fcc0a7abed50ddc5dc4d1623e1c28ff6
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-06 08:52:01 -0300

    Requirement 7

--- fundamentals/block-08/day-3/part-1/exercise7.js
+++ fundamentals/block-08/day-3/part-1/exercise7.js
@@ -69,6 +69,7 @@ const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
   // escreva seu código aqui
+  return books.find((book) => book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3).name;
 }
 
 assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
