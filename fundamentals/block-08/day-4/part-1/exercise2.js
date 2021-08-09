c50e241db37c1f8425649dd91fd06e797adb62aa
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-09 10:43:07 -0300

    Exercise 2

--- fundamentals/block-08/day-4/part-1/exercise2.js
+++ fundamentals/block-08/day-4/part-1/exercise2.js
@@ -69,6 +69,11 @@ const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Fra
 
 function reduceNames() {
   // escreva seu código aqui
+  const names = books.reduce((acc, book, index, array) => {
+    if(index === array.length - 1) return `${acc} ${book.author.name}.`;
+    return `${acc} ${book.author.name},`
+  }, '');
+  return names.trim();
 }
 
 assert.strictEqual(reduceNames(), expectedResult);
