f91a55a26ac438ab3d317c8a351b18064ccb4537
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-10 10:51:36 -0300

    Requirement 2 exercise Array Destructuring

--- fundamentals/block-08/day-5/part-1/arrayDestructuring.js
+++ fundamentals/block-08/day-5/part-1/arrayDestructuring.js
@@ -11,7 +11,7 @@ realizaSaudacao(saudacao);
 let comida = 'gato';
 let animal = 'água';
 let bebida = 'arroz';
-
+[comida, animal, bebida] = ['arroz', 'gato', 'água'];
 console.log(comida, animal, bebida); // arroz gato água
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
