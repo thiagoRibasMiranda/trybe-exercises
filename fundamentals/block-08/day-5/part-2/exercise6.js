37562b1ec8995abf7801a1753cfa41cdb0e1182f
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-11 09:37:22 -0300

    Exercise6

--- fundamentals/block-08/day-5/part-2/exercise6.js
+++ fundamentals/block-08/day-5/part-2/exercise6.js
@@ -6,6 +6,7 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+const toObject = ([name, brand, year]) => ({name, brand, year});
 
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
