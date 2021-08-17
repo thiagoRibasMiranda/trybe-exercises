cf90a27eee17bfde04e47beebb3b1ae20b1f2f95
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-17 09:26:02 -0300

    Exercise 5

--- fundamentals/block-09/day-1/part-2/exercise5.js
+++ fundamentals/block-09/day-1/part-2/exercise5.js
@@ -15,6 +15,10 @@ const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  const currentTemperature = getMarsTemperature();
+  callback(currentTemperature);
+};
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
