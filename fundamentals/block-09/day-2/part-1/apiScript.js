dde79b467c1aae1a4c07e4f824a54df01a6a7bb4
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-25 09:00:56 -0300

    Initial commit 9.2

--- fundamentals/block-09/day-2/part-1/apiScript.js
+++ fundamentals/block-09/day-2/part-1/apiScript.js
@@ -1,14 +1,16 @@
-// apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
   const myObject = {
     method: 'GET',
-    headers: { 'Accept': 'application/json' }
+    headers: { Accept: 'application/json' },
   };
 
-  fetch(API_URL, myObject);
-};
-
-window.onload = () => fetchJoke();
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => {
+      // Exercise 1 - add a joke to the page
+      document.getElementById('jokeContainer').innerHTML = data.joke;
+    });
+  };
+  window.onload = () => fetchJoke();
