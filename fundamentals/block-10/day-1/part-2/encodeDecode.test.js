e3048ab17fb70373ad9fbf6673cc251bbfdcf0f7
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-26 20:54:56 -0300

    Requirement 2.2 par-2

--- fundamentals/block-10/day-1/part-2/encodeDecode.test.js
+++ fundamentals/block-10/day-1/part-2/encodeDecode.test.js
@@ -7,4 +7,20 @@ describe('Testa as funções encode e decode', () => {
   it('decode é uma função', () => {
     expect(typeof decode).toStrictEqual('function');
   });
+  it('encode converte a vogal "a" no número 1', () => {
+    expect(encode('a')).toStrictEqual('1');
+  });
+  it('encode converte a vogal "e" no número 2', () => {
+    expect(encode('e')).toStrictEqual('2');
+  });
+  it('encode converte a vogal "i" no número 3', () => {
+    expect(encode('i')).toStrictEqual('3');
+  });
+  it('encode converte a vogal "o" no número 4', () => {
+    expect(encode('o')).toStrictEqual('4');
+  });
+  it('encode converte a vogal "u" no número 5', () => {
+    expect(encode('u')).toStrictEqual('5');
+  });
+
 });
