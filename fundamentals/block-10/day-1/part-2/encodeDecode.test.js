1f53fd4385a5f6f791a6cd7f85533058f5026aff
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-08-26 21:07:21 -0300

    Requirement 1.4  part-2

--- fundamentals/block-10/day-1/part-2/encodeDecode.test.js
+++ fundamentals/block-10/day-1/part-2/encodeDecode.test.js
@@ -38,4 +38,36 @@ describe('Testa as funções encode e decode', () => {
   it('decode converte o número 5 na vogal "u"', () => {
     expect(decode('5')).toStrictEqual('u');
   }); 
+
+  it('encode converte apenas a vogal "a" no número 1', () => {
+    expect(encode('ana')).toStrictEqual('1n1');
+  });
+  it('encode converte apenas a vogal "e" no número 2', () => {
+    expect(encode('ele')).toStrictEqual('2l2');
+  });
+  it('encode converte apenas a vogal "i" no número 3', () => {
+    expect(encode('xixi')).toStrictEqual('x3x3');
+  });
+  it('encode converte apenas a vogal "o" no número 4', () => {
+    expect(encode('ovo')).toStrictEqual('4v4');
+  });
+  it('encode converte apenas a vogal "u" no número 5', () => {
+    expect(encode('nu')).toStrictEqual('n5');
+  });
+
+  it('decode converte apenas o número 1 na vogal "a"', () => {
+    expect(decode('1n1')).toStrictEqual('ana');
+  });
+  it('decode converte apenas o número 2 na vogal "e"', () => {
+    expect(decode('2l2')).toStrictEqual('ele');
+  });
+  it('decode converte apenas o número 3 na vogal "i"', () => {
+    expect(decode('x3x3')).toStrictEqual('xixi');
+  });
+  it('decode converte apenas o número 4 na vogal "o"', () => {
+    expect(decode('4v4')).toStrictEqual('ovo');
+  });
+  it('decode converte apenas o número 5 na vogal "u"', () => {
+    expect(decode('n5')).toStrictEqual('nu');
+  }); 
 });
