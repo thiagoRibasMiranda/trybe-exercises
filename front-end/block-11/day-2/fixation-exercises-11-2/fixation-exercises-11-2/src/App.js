73963e11953fa85ca5fdf2e67d9ab1351cbe76e3
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-03 08:20:38 -0300

    Initial commit part-2 of fixation exercises 11-2

--- front-end/block-11/day-2/fixation-exercises-11-2/fixation-exercises-11-2/src/App.js
+++ front-end/block-11/day-2/fixation-exercises-11-2/fixation-exercises-11-2/src/App.js
@@ -1,13 +1,37 @@
-// import logo from './logo.svg';
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
-import Image from './Image';
+import Order from './Order';
 
-function App() {
-  return (
-    <main>
-      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         {/*  adicione os componentes aqui */}
+      </div>
+    );
+  }
 }
 
 export default App;
