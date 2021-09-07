5c4582a753f5b4ec56b335d61a44bfcdace59f56
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-07 11:32:24 -0300

    Requirement 4

--- front-end/block-12/fixation-exercises-12-1/fancy-buttons/src/App.js
+++ front-end/block-12/fixation-exercises-12-1/fancy-buttons/src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import React from 'react'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+  }
+
   handleClick() {
     console.log('Texto qualquer', this);
   }
