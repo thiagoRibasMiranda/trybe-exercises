fe5ce341be4dd9fbbcb68ac98245b95fa8e2b47d
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-07 12:14:57 -0300

    Requirement 5, 6,7

--- front-end/block-12/fixation-exercises-12-1/fancy-buttons/src/App.js
+++ front-end/block-12/fixation-exercises-12-1/fancy-buttons/src/App.js
@@ -4,29 +4,40 @@ import React from 'react'
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      numeroCliques1: 0,
+      numeroCliques2: 0,
+      numeroCliques3: 0,
+    }
     this.handleClick = this.handleClick.bind(this)
     this.handleClick2 = this.handleClick2.bind(this)
     this.handleClick3 = this.handleClick3.bind(this)
   }
 
-  handleClick() {
-    console.log('Texto qualquer', this);
+  handleClick = () => {
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliques1: estadoAnterior.numeroCliques1 + 1,
+    }));
   }
 
   handleClick2() {
-    console.log('Texto qualquer 2', this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliques2: estadoAnterior.numeroCliques2 + 1
+    }));
   }
 
   handleClick3() {
-    console.log('Texto qualquer 3', this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliques3: estadoAnterior.numeroCliques3 + 1
+    }))
   }
 
   render() {
     return (
       <>
-      <button onClick={this.handleClick}>Botao1</button>
-      <button onClick={this.handleClick2}>Botao2</button>
-      <button onClick={this.handleClick3}>Botao3</button>
+      <button onClick={this.handleClick}>Botao1 | {this.state.numeroCliques1}</button>
+      <button onClick={this.handleClick2}>Botao2 | {this.state.numeroCliques2}</button>
+      <button onClick={this.handleClick3}>Botao3 | {this.state.numeroCliques3}</button>
       </>
     )
   }
