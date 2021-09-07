5e7735cccd641dded3fba1856e3df47fa61f2db2
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-07 10:44:46 -0300

    Requirement 2

--- front-end/block-12/fixation-exercises-12-1/fancy-buttons/src/App.js
+++ front-end/block-12/fixation-exercises-12-1/fancy-buttons/src/App.js
@@ -4,11 +4,21 @@ import React from 'react'
 function handleClick() {
   console.log('Texto qualquer');
 }
+
+function handleClick2() {
+  console.log('Texto qualquer 2');
+}
+
+function handleClick3() {
+  console.log('Texto qualquer 3');
+}
 class App extends React.Component {
   render() {
     return (
       <>
       <button onClick={handleClick}>Botao1</button>
+      <button onClick={handleClick2}>Botao2</button>
+      <button onClick={handleClick3}>Botao3</button>
       </>
     )
   }
