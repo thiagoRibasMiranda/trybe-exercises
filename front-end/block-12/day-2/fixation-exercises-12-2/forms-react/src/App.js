75d9ce1f118ff9b241bdb6a7cb937412f8a2d068
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-09 10:52:29 -0300

    Requirement 1 & 2

--- front-end/block-12/day-2/fixation-exercises-12-2/forms-react/src/App.js
+++ front-end/block-12/day-2/fixation-exercises-12-2/forms-react/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeAno = this.handleChangeAno.bind(this);
+    this.toggleChange = this.toggleChange.bind(this);
+    this.handleChangeMarca = this.handleChangeMarca.bind(this);
+
+    this.state = {
+      descrição: '',
+      ano: 0,
+      isChecked: false,
+      marca: '',
+    };
+  }
+    handleChange(event) {
+      this.setState({
+        descrição: event.target.value,
+      });
+    }
+    handleChangeAno(event) {
+      this.setState({
+        ano: event.target.value,
+      })
+    }
+    toggleChange = () => {
+      this.setState({
+        isChecked: !this.state.isChecked,
+      });
+    }
+    handleChangeMarca(event) {
+      this.setState({
+        marca: event.target.value,
+      })
+    }
+  render() {    
+    return (
+      <>
+      <label>
+        Marca:
+        <select value={this.state.marca} onChange={this.handleChangeMarca}>
+          <option>Volvo</option>
+          <option>Audi</option>
+          <option>Mercedes</option>
+        </select> <br/>
+      </label>
+      
+      <label>
+        Ano:
+        <input type="number" name="ano" value={this.state.ano} onChange={this.handleChangeAno}></input> <br/>
+      </label>
+      
+      <label>
+        Gasolina:
+        <input type="checkbox" name="gasolina" checked={this.state.isChecked} onChange={this.toggleChange}></input> <br/>
+      </label>
+
+      <label>
+        Descrição:
+        <textarea name="descrição" value={this.state.descrição} onChange={this.handleChange}></textarea>
+      </label>
+      
+    </>
+    )
+    
+  }
 }
 
 export default App;
