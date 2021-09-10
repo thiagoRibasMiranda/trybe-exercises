800a713b009eec83c6a2a14a41c86e3ea67d432f
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-10 08:08:19 -0300

    Initial commit my-form-2.0

---
+++ front-end/block-12/day-2/my-form-2.0/my-form-2.0/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Form from './Form';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+ }
+  
+}
+
+export default App;
---
+++ front-end/block-12/day-2/my-form-2.0/my-form-2.0/src/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor() {
+    super();
+
+  }
+  render() {
+    return (
+      <fieldset>
+        <input type="text"></input>
+      </fieldset>
+    );
+ }
+  
+}
+
+export default Form;
