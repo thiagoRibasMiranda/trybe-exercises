fcd9e9d26b0a51525ad69d693f6c161825b4484d
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-15 16:29:53 -0300

    Added function toggleDisplayCronometer

--- front-end/block-13/day-1/cronometer/my-cronometer-app/src/App.js
+++ front-end/block-13/day-1/cronometer/my-cronometer-app/src/App.js
@@ -3,14 +3,29 @@ import './App.css';
 import Cronometer from './components/Cronometer';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showCronometer: true,
+    }
+  }
+
+  toggleDisplayCronometer = () => {
+    this.setState((prevState) => ({
+      showCronometer: !prevState.showCronometer,
+    }))
+  }
+
   render() {
+    const { showCronometer } = this.state;
     return (
       <main>
         <section>
-          <Cronometer />
+          { showCronometer ? <Cronometer /> : '' }
         </section>
-          <button type="button" onClick={ () => {} }> 
-            Desligar cronômetro
+          <button type="button" onClick={ this.toggleDisplayCronometer }> 
+          { showCronometer ? 'Desligar cronometro' : 'Ligar cronometro' }
           </button>
       </main>
     );
