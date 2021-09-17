6c002c8675e32229c221588b02bea85ffe0ec907
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-16 22:52:59 -0300

    Added function fetchApi

--- front-end/block-13/day-1/fixation-exercises-13-1/my-radomuser-app/src/App.js
+++ front-end/block-13/day-1/fixation-exercises-13-1/my-radomuser-app/src/App.js
@@ -3,13 +3,40 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super()
-
+    this.state = {
+      api: '',     
+      loading: true,
+      name: ''
+    };
+    this.changeDataJson = this.changeDataJson.bind(this);
+    // this.componentDidMount = this.componentDidMount.bind(this);
   }
+
+  async componentDidMount() {
+    try {
+      const url = `https://api.randomuser.me`;
+      const response = await fetch(url);
+      const dataJson = await response.json();
+      this.changeDataJson(dataJson);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  changeDataJson(dataJson) {
+    this.setState({ api: dataJson.results, name: dataJson.results[0].name.first })
+  }
+
   render() {
+    // const { api, loading } = this.state;
+    // const name = api.name;
+    // const firstName = name.first;
     return(
         <>
         <div>
-          <h3>loading...</h3>
+          {/* <h3>loading...</h3> */}
+          {/* <p>{ api.name.first }</p> */}
+          {/* { !loading ? <p>loading...</p> : <p> { name } </p>} */}
         </div>
         </>
     )
