4f235a6b52aacef7f643908961650b75cc2d4894
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-16 21:36:17 -0300

    Initial commit my-randomuser-app

---
+++ front-end/block-13/day-1/fixation-exercises-13-1/my-radomuser-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+  }
+  render() {
+    return(
+        <>
+        <div>
+          <h3>loading...</h3>
+        </div>
+        </>
+    )
+
+  }
+}
+
+export default App;
