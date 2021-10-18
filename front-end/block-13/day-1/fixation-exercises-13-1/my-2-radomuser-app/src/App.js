9d1d927d3a50ff3ae5c27d2392455750a38efd21
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-10-18 11:52:22 -0300

    Added function fetchApi

--- front-end/block-13/day-1/fixation-exercises-13-1/my-2-radomuser-app/src/App.js
+++ front-end/block-13/day-1/fixation-exercises-13-1/my-2-radomuser-app/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      userObj: undefined,
+      loading: true,
+      userImg: '',
+      userName: '',
+      userEmail: '',
+      userAge: '',
+    };
+  }
+
+  async fetchApi() {
+    this.setState(
+      {loading: true },
+      async () => {
+        const requestReturn = await fetch ('https://api.randomuser.me/');
+        const requestObject = await requestReturn.json();
+        this.setState({
+          loading: false,
+          userObj: requestObject
+        });
+      }
+    );
+  }
+  
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
