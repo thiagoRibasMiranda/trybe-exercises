9455594810bd5d0cb414de85c26f25fa65b7c3d0
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-11-25 11:06:14 -0300

    Requirement 4

--- front-end/block-14/day-1/fixation-exercises-14-1/testes-react/src/App.js
+++ front-end/block-14/day-1/fixation-exercises-14-1/testes-react/src/App.js
@@ -1,16 +1,48 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <label htmlFor="id-email">
-        Email
-        <input id="id-email" type="email"/>
-      </label>
-      <input id="btn-send" type="button" value="Enviar" data-testid="id-send" />
-      <input id="btn-back" type="button" value="Voltar" />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      saveEMail: '',
+    }
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: ''});
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+          <input
+            id="id-email"
+            value={ email }
+            type="email"
+            onChange={(e) => this.changeEmail(e.target.value)}
+          />
+        </label>
+        <input
+          id="btn-send"
+          type="button"
+          value="Enviar"
+          data-testid="id-send"
+          onClick={ () => this.changeSaveEmail(email)}
+        />
+        <input id="btn-back" type="button" value="Voltar" />
+        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
