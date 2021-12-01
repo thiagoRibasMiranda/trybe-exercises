31f94ec8638da5eef7d6721ea60cd9a0599b3bc6
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-12-01 12:20:44 -0300

    Requirement 4

--- front-end/block-14/day-2/fixation-exercises-14-2/fixation-exercise-test-mocks-inputs/src/App.test.js
+++ front-end/block-14/day-2/fixation-exercises-14-2/fixation-exercise-test-mocks-inputs/src/App.test.js
@@ -1,31 +1,51 @@
 // App.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-afterEach(() => jest.clearAllMocks());
-
-it('fetches a joke', async () => {
-  const joke = {
-    id: '7h3oGtrOfxc',
-    joke: 'Whiteboards ... are remarkable.',
-    status: 200,
-  };
-
-  global.fetch = jest.fn(async () => ({
-    json: async () => joke
-  }));
-
-  // global.fetch = jest.fn(() => Promise.resolve({
-  //   json: () => Promise.resolve(joke),
-  // }));
-
+test('alterando o valor dos campos e testando o valor guardado', () => {
   render(<App />);
-  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
-  expect(renderedJoke).toBeInTheDocument();
-  expect(global.fetch).toBeCalledTimes(1);
-  expect(global.fetch).toBeCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+  const inputNome = screen.getByRole('textbox', { name: /nome/i });
+  expect(inputNome).toBeInTheDocument();
+  expect(inputNome).toHaveValue('');
+  userEvent.type(inputNome, 'Estudante da Trybe');
+  expect(inputNome).toHaveValue('Estudante da Trybe');
+
+  const inputEmail = screen.getByRole('textbox', { name: /email/i });
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveValue('');
+  userEvent.type(inputEmail, 'estudante@trybe.com');
+  expect(inputEmail).toHaveValue('estudante@trybe.com');
 });
+// // App.test.js
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// afterEach(() => jest.clearAllMocks());
+
+// it('fetches a joke', async () => {
+//   const joke = {
+//     id: '7h3oGtrOfxc',
+//     joke: 'Whiteboards ... are remarkable.',
+//     status: 200,
+//   };
+
+//   global.fetch = jest.fn(async () => ({
+//     json: async () => joke
+//   }));
+
+//   // global.fetch = jest.fn(() => Promise.resolve({
+//   //   json: () => Promise.resolve(joke),
+//   // }));
+
+//   render(<App />);
+//   const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+//   expect(renderedJoke).toBeInTheDocument();
+//   expect(global.fetch).toBeCalledTimes(1);
+//   expect(global.fetch).toBeCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+// });
 // // App.test.js
 // import React from 'react';
 // import { render, screen } from '@testing-library/react';
