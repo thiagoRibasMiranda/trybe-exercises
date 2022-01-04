c84763fe49f8a3b25cce4f83ae0747b32c9de520
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-01-04 12:37:06 -0300

    Requirement 1

--- front-end/block-15/day-1/fixation-exercises-15-1/First-exercise-Redux/index.js
+++ front-end/block-15/day-1/fixation-exercises-15-1/First-exercise-Redux/index.js
@@ -1,1 +1,15 @@
 const Redux = require('redux');
+
+const ESTADO_INICIAL = {
+  login: false,
+  email: "",
+} 
+
+const reducer = (state = ESTADO_INICIAL) => {
+  // console.log(state);
+  return state;
+};
+  
+const store = Redux.createStore(reducer);
+
+console.log(store.getState());
