154e659bec8e02f27640154c4c88a35ae5373555
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-01-11 11:45:03 -0300

    Requirement 1 and 2

--- front-end/block-15/day-2/fixation-exercise-15-2/my-app/src/store/index.js
+++ front-end/block-15/day-2/fixation-exercise-15-2/my-app/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
-
+const extension = window.devToolsExtension() || ((f) => f);
+const store = createStore(rootReducer, extension);
+ 
 export default store;
