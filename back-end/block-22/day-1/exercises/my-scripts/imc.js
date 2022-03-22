1767438bcec9951d4e47cb6f165531814723d324
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-22 17:59:34 -0300

    feat: adicionado o pacote readline-sync e implementado na funcao imc

--- back-end/block-22/day-1/exercises/my-scripts/imc.js
+++ back-end/block-22/day-1/exercises/my-scripts/imc.js
@@ -1,5 +1,10 @@
-const peso = 95;
-const altura = 183;
+const readline = require('readline-sync');
+
+// const peso = 95;
+// const altura = 183;
+
+const peso = readline.question('Qual é o seu peso? ');
+const altura = readline.questionInt('Qual é a sua altura em centimetros? ');
 
 const imc = (peso, altura) => {
   const resultado = (peso / ((altura/100) * (altura/100))).toFixed(2);
