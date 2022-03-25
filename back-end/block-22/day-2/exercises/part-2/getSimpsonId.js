7585b548edddc6f04235f44b32f91e4adf31b2df
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-25 13:17:50 -0300

    feat:implementada nova funcinalidade a funcao getSimpsonId

--- back-end/block-22/day-2/exercises/part-2/getSimpsonId.js
+++ back-end/block-22/day-2/exercises/part-2/getSimpsonId.js
@@ -4,9 +4,11 @@ async function getSimpsonId(id) {
   const simpsons = await fs
     .readFile('./simpsons.json', 'utf-8')
     .then((data) => JSON.parse(data));
-  if(simpsons.filter((element) => element.id === id).length === 0) {
+  const chosenSimpson = simpsons.filter((element) => element.id === id);
+  if(chosenSimpson.length === 0) {
     throw new Error('id não encontrado');
   };
+  return console.log(chosenSimpson);
 };
 
 getSimpsonId("11");
