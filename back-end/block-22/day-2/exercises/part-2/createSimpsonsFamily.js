576e85e74ea33f13eb533a9dd3d406b7b250ef02
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-26 12:33:44 -0300

    feat: modificacoes na  funcao createSimpsonsFamily

--- back-end/block-22/day-2/exercises/part-2/createSimpsonsFamily.js
+++ back-end/block-22/day-2/exercises/part-2/createSimpsonsFamily.js
@@ -5,9 +5,7 @@ async function createSimpsonsFamily() {
     .readFile('./simpsons.json', 'utf-8')
     .then((fileContent) => JSON.parse(fileContent));
 
-  console.log(simpsons);
   const simpsonsFamily = simpsons.filter(simpson => [1, 2, 3, 4].includes(simpson.id));
-  console.log(simpsonsFamily);
 
   await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
 }
