b059fd3155813b56c5c6671849577cd6ff344977
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-27 19:26:15 -0300

    feat: created route POST /hello

--- back-end/block-22/day-3/exercises/part-1/index.js
+++ back-end/block-22/day-3/exercises/part-1/index.js
@@ -4,9 +4,14 @@ const bodyParser = require('body-parser');
 const app = express(); 
 app.use(bodyParser.json());
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.json({ message: 'pong' });
-})
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello:, ${name}!`});
+});
 
 app.use(function (err, req, res, next) {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
