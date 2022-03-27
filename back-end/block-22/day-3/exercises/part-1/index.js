d548a7015bb91f0e43296699116ce6c2773e5a51
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-27 19:20:51 -0300

    feat: created route GET /ping

---
+++ back-end/block-22/day-3/exercises/part-1/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express(); 
+app.use(bodyParser.json());
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+})
+
+app.use(function (err, req, res, next) {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
+
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
