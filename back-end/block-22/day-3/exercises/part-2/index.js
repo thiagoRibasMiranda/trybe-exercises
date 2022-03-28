88d64fdafc17dd99c40911c860b03a3ff15487ff
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-28 13:00:53 -0300

    feat: created route Get /simpsons

--- back-end/block-22/day-3/exercises/part-2/index.js
+++ back-end/block-22/day-3/exercises/part-2/index.js
@@ -1,2 +1,21 @@
-const fs = require('fs').promises;
+const express = require('express');
+const bodyParser = require('body-parser');
+const { getSimpsons } = require('./fs-utils');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/simpsons', async (_req, res) => {
+  const simpsons = await getSimpsons();
+  console.log(simpsons);
+  if(!simpsons) return res.status(500);
+  res.status(200).json(simpsons);
+})
+
+app.all('*', function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+});
+
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
+
 
