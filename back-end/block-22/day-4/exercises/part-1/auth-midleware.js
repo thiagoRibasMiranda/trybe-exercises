1974372a2d80d55e23c5e1f8c7e1ca0dc6ec63ab
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-29 19:31:49 -0300

    feat: created auth-midleware

---
+++ back-end/block-22/day-4/exercises/part-1/auth-midleware.js
@@ -0,0 +1,10 @@
+const authMiddleware = (req, res, next) => {
+  const { email, password } = req.body;
+  const re = /\S+@\S+\.\S+/;
+  if( !re.test(email) ) return res.status(400).json({ "message": "invalid data" });
+  if( password.length < 4 || password > 8) return res.status(400).json({ "message": "invalid data" });
+  res.status(201).json({ "message": "user created" });
+  next();
+}
+
+module.exports(authMiddleware);
