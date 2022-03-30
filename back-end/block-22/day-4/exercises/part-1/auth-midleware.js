62e2736c687a6015c638b0f5d3f742c2dffd828e
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-29 21:16:52 -0300

    feat: created route POST /user/login

--- back-end/block-22/day-4/exercises/part-1/auth-midleware.js
+++ back-end/block-22/day-4/exercises/part-1/auth-midleware.js
@@ -1,9 +1,9 @@
 const authMiddleware = (req, res, next) => {
   const { email, password } = req.body;
   const re = /\S+@\S+\.\S+/;
-  if( !re.test(email) ) return res.status(400).json({ "message": "invalid data" });
-  if( password.length < 4 || password > 8) return res.status(400).json({ "message": "invalid data" });
+  if( !re.test(email) ) return res.status(400).json({ "message": "invalid data email" });
+  if( password.length < 4 || password > 8) return res.status(400).json({ "message": "invalid data password" });
   next();
 }
 
-module.exports(authMiddleware);
+module.exports = authMiddleware;
--- back-end/block-22/day-4/exercises/part-1/index.js
+++ back-end/block-22/day-4/exercises/part-1/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const authMiddleware = require('./auth-midleware');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/user/register', authMiddleware, function (req, res) {
+app.post('/user/register',authMiddleware, function (req, res) {
   const { username } = req.body;
-  if( username.length < 4 ) return res.status(400).json({ "message": "invalid data" });
+  if( username.length < 4 ) return res.status(400).json({ "message": "invalid data username" });
   res.status(201).json({ "message": "user created" });
 });
 
+app.post('/user/login',authMiddleware, function (_req, res) {
+  const token = Math.random().toString(16).substr(3);
+  res.status(200).json({ "token": token });
+});
+
+app.all('*', function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+});
+
+app.listen(3001, () => {
+console.log('Aplicação ouvindo na porta 3001');
+});
