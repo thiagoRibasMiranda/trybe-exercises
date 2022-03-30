0219d541e4c522fab16c933a83cbb48aa1cd3937
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-03-30 11:12:39 -0300

    feat: created route GET /btc/price

--- back-end/block-22/day-4/exercises/part-1/index.js
+++ back-end/block-22/day-4/exercises/part-1/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const authMiddleware = require('./auth-midleware');
+const {authMiddleware, isValidToken} = require('./auth-midleware');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/user/register',authMiddleware, function (req, res) {
+app.post('/user/register', authMiddleware, function (req, res) {
   const { username } = req.body;
   if( username.length < 4 ) return res.status(400).json({ "message": "invalid data username" });
   res.status(201).json({ "message": "user created" });
 });
 
-app.post('/user/login',authMiddleware, function (_req, res) {
+app.post('/user/login', authMiddleware, function (_req, res) {
   const token = Math.random().toString(16).substr(3);
   res.status(200).json({ "token": token });
+});
+
+app.get('/btc/price', isValidToken, async function (req, res) {
+  const ENDPOINTEXTERNALAPI = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
+  const result = await axios.get(ENDPOINTEXTERNALAPI);
+
+  res.status(200).json(result.data);
 });
 
 app.all('*', function (req, res) {
