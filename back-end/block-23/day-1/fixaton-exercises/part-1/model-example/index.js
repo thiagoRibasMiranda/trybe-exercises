43b107d4a03ed4ad616b1da11a0e39dfe4017877
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-04-08 17:20:12 -0300

    feat: created route POST /books

--- back-end/block-23/day-1/fixaton-exercises/part-1/model-example/index.js
+++ back-end/block-23/day-1/fixaton-exercises/part-1/model-example/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 const Author = require('./models/Author');
 const Book = require('./models/Book');
+
+app.use(bodyParser.json());
 
 app.get('/authors', async(req, res) => {
   const authors = await Author.getAll();
@@ -22,8 +25,42 @@ app.get('/books', async(req, res) => {
 app.get('/books/:id', async(req, res) => {
   const { id } = req.params;
   const books = await Book.getByAuthorId(id);
-
+  if(!books) return res.status(404).json({ message: 'Not found'});
   res.status(200).json(books);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
+
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body;
+  console.log(title);
+  if (!Book.isValid(title, author_id)) {
+      return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Book.create(title, author_id);
+
+  res.status(201).json({ message: 'Autor criado com sucesso! '});
 });
 
 app.listen(PORT, () => {
