781c39f07d802ce62436cfdd10831511f27b505f
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-04-26 21:30:38 -0300

    Criada a estrutura inicial do exercicio

---
+++ back-end/block-24/day-1/exercises/part-1/api-with-sequelize/index.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(bodyParser.json());
+
+// app.use('/book', bookController);
+
+// app.listen(PORT, () => console.log(`Ouvindo n aporta ${PORT}!`));
