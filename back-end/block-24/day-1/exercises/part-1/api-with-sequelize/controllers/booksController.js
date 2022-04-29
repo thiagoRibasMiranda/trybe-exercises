8983d34a378d703e95eb77df8d9c814d135eefec
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-04-29 13:04:07 -0300

    Criado o endpoint post /

--- back-end/block-24/day-1/exercises/part-1/api-with-sequelize/controllers/booksController.js
+++ back-end/block-24/day-1/exercises/part-1/api-with-sequelize/controllers/booksController.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models')
+const { Book }= require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const books = await Book.findAll;
-    return res.status(200).json('teste')
+    const books = await Book.findAll();
+    return res.status(200).json(books);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
@@ -17,6 +17,17 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params;
     const book = await Book.findByPk(id);
     return res.status(200).json(book)
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, author, pageQuantity } = req.body;
+    const newBook = await Book.create({ title, author, pageQuantity });
+    return res.status(201).json(newBook)
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
