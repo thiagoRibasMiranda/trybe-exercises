e66cea63a2412635a9426948cb105e4c21fc29a7
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-06-04 09:56:36 -0300

    exercise 5 27.2

--- back-end/block-27/exercises/index.js
+++ back-end/block-27/exercises/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Person_1 = __importDefault(require("./Person"));
 const Student_1 = __importDefault(require("./Student"));
 const Subject_1 = __importDefault(require("./Subject"));
+const Teacher_1 = __importDefault(require("./Teacher"));
 const casimiro = new Person_1.default('Casimiro', new Date('1980/01/25'));
 const gaules = new Student_1.default('Gau', new Date('1944/04/04'));
 gaules.examsGrades = [41, 44, 42, 45];
@@ -24,3 +25,5 @@ const testInterfaceEmployee = {
 console.log(testInterfaceEmployee);
 const math = new Subject_1.default('Matemática');
 console.log(math);
+const marta = new Teacher_1.default('Marta da Silva', new Date('1980/03/30'), 2000, math);
+console.log(marta);
--- back-end/block-27/exercises/index.ts
+++ back-end/block-27/exercises/index.ts
@@ -2,6 +2,7 @@ import Person from "./Person";
 import Student from "./Student";
 import IEmployee from "./Employee";
 import Subject from "./Subject";
+import Teacher from "./Teacher";
 
 const casimiro = new Person('Casimiro',new Date('1980/01/25'));
 const gaules = new Student('Gau', new Date('1944/04/04'));
@@ -30,3 +31,7 @@ console.log(testInterfaceEmployee);
 const math = new Subject('Matemática');
 
 console.log(math);
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+
+console.log(marta);
